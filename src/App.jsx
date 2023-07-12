c96b14cff23c436b10cbd5f6855f9466b796c285
mmya2300 <mmya230300@gmail.com>
2023-07-12 11:38:26 -0400

    Added Tailwind CSS 7/12/23

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,8 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className = "font-bold text-blue-500" >
+        Hey, Mya here! I added Tailwind CSS to our website. Just place the Tailwind Components into the className section! 
       </p>
     </>
   )
