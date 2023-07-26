04a4db66025638c5748e2de4d560eb17886cc943
Jaden Lewis-Chambers <you@example.com>
2023-07-26 18:38:27 -0400

    added hero.jsx file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <p className = "font-bold text-blue-500" >
+    Hey, Mya here! I added Tailwind CSS to our website. Just place the Tailwind Components into the className section! 
+  </p>
   )
 }
 
