2dd5a29334ff3bbca83249f4f8ce48a0a334406b
Jaden Lewis-Chambers <you@example.com>
2023-07-31 13:36:28 -0400

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
+    <main className="bg-[#0B0B0E]	h-full p-0 m-0 box-border">
+      <div className="min-h-full w-auto">
       <Hero/>
+      </div>
+    </main>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,14 @@
 function Hero() {
   return (  
     <>
-    <section>
-      <h1>Introducing Design & Code</h1>
-      <p>We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
-      <button>Become a member</button>
+    <section className=" content-center items-center justify-center flex-col flex py-8 max-w-full h-screen">
+      <div className="content-center items-center justify-center flex-wrap flex-col flex max-w-7xl">
+        <div className="w-full max-w-lg gap-8 flex-wrap flex-col flex">
+          <h1 className="text-white text-center text-5xl font-bold">Introducing <br/>Design & Code</h1>
+          <p className="text-[#BBBBBB] text-xl text-center">We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
+        </div>
+          <button className="bg-white font-bold py-4 px-8 rounded-xl mt-8">Become a member</button>
+      </div>
     </section>
     </>
   )
