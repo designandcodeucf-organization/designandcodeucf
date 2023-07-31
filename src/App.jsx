0634c077c45188503b027f903ac13b20c0ac872b
Jaden Lewis-Chambers <you@example.com>
2023-07-31 15:24:30 -0400

    added framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <main className="bg-[#0B0B0E]	h-full p-0 m-0 box-border">
+    <main className="	h-full p-0 m-0 box-border">
       <div className="min-h-full w-auto">
       <Hero/>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,16 @@
 function Hero() {
   return (  
     <>
-    <section className=" content-center items-center justify-center flex-col flex py-8 max-w-full h-screen">
-      <div className="content-center items-center justify-center flex-wrap flex-col flex max-w-7xl">
-        <div className="w-full max-w-lg gap-8 flex-wrap flex-col flex">
-          <h1 className="text-white text-center text-5xl font-bold">Introducing <br/>Design & Code</h1>
-          <p className="text-[#BBBBBB] text-xl text-center">We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
+    <section className=" relative content-center items-center justify-center flex-col flex py-8 max-w-full h-screen">
+      <div>
         </div>
+        <div className="content-center items-center justify-center flex-wrap flex-col flex max-w-7xl">
+          <div className="w-full max-w-lg gap-8 flex-wrap flex-col flex">
+            <h1 className="text-white text-center text-5xl font-bold">Introducing <br/>Design & Code</h1>
+            <p className="text-[#BBBBBB] text-xl text-center">We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
+          </div>
           <button className="transition duration-300 hover:scale-105 bg-white font-bold rounded-xl py-4 px-8 mt-8">Become a member</button>
-          
-      </div>
-
+        </div>
     </section>
     </>
   )
