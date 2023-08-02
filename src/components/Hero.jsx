eb469264b07e1d1434eb2f5f87d44740ab73b4e1
Jaden Lewis-Chambers <you@example.com>
2023-08-02 19:25:53 -0400

    added button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ function Hero() {
       <h1>Introducing Design & Code</h1>
       <p>We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
       <button>Become a member</button>
+      <button>new-branch button</button>
     </section>
     </>
   )
