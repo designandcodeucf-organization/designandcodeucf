4e96b60da9937007f43f9c16bad29d011bc36425
jaden416 <jaden416@outlook.com>
2023-08-06 18:11:16 -0400

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ function App() {
 
   return (
     <>
-    <main className="	h-full p-0 m-0 box-border">
-      <div className="min-h-full w-auto">
-      <Hero/>
+    <main className="h-full box-border m-0 p-0">
+      <div className="contents">
+        <div style={{minHeight:"100%", width:"auto"}}className="relative flex  flex-col flex-nowrap gap-6 justify-start overflow-hidden p-0 min-h-full h-min">
+          <Hero/>
+        </div>
       </div>
     </main>
     </>
@@ -14,3 +16,4 @@ function App() {
 }
 
 export default App
+//relative flex flex-col flex-nowrap w-auto overflow-hidden justify-start h-min	gap-6
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,37 @@
 function Hero() {
   return (  
     <>
-    <section className=" relative content-center items-center justify-center flex-col flex py-8 max-w-full h-screen">
+      <section className="relative flex flex-col flex-none flex-nowrap overflow-visible content-center items-center justify-center pt-52 h-min w-full ">
+        {/* hero inner */}
+        <div className="relative flex flex-col flex-none flex-wrap content-center overflow-visible items-center justify-center px-4 h-min max-w-7xl w-full">
+          {/* heading & cta */}
+          <div className="z-2 relative flex flex-col flex-none flex-nowrap content-center overflow-visible items-center justify-center px-4 h-min max-w-md w-full">
+            <div className="relative flex flex-col flex-none flex-nowrap items-center justify-center gap-8 overflow-hidden h-min max-w-lg	 w-full">
+              <div className="relative flex flex-col flex-none shrink-0	break-words whitespace-pre-wrap justify-start h-auto w-full">
+                <h1 className="text-white text-center text-5xl font-bold">Introducing <br/>Design & Code</h1>
+              </div>
+              <div className="relative flex flex-col flex-none shrink-0	break-words whitespace-pre-wrap justify-start h-auto w-full">
+                <p className="text-[#BBBBBB] text-xl text-center font-medium">We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
+              </div>
+            </div>
+
+            {/* <div className="flex items-center justify-center gap-10 h-min-content overflow-hidden relative w-min-content"> */}
+              <button className="transition duration-150 hover:scale-105 bg-white font-bold rounded-xl py-4 px-8 mt-8">Become a member</button>
+            {/* </div>s */}
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Hero
+
+/*
+function Hero() {
+  return (  
+    <>
+    <section className=" zrelative content-center items-center justify-center flex-col flex py-8 max-w-full h-screen">
       <div>
         </div>
         <div className="content-center items-center justify-center flex-wrap flex-col flex max-w-7xl">
@@ -10,7 +40,7 @@ function Hero() {
             <h1 className="text-white text-center text-5xl font-bold">Introducing <br/>Design & Code</h1>
             <p className="text-[#BBBBBB] text-xl text-center">We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
           </div>
-          <button className="transition duration-300 hover:scale-105 bg-white font-bold rounded-xl py-4 px-8 mt-8">Become a member</button>
+          <button className="transition duration-150 hover:scale-105 bg-white font-bold rounded-xl py-4 px-8 mt-8">Become a member</button>
         </div>
     </section>
     </>
@@ -18,3 +48,5 @@ function Hero() {
 }
 
 export default Hero
+
+*/
