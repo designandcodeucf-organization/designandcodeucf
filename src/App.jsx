f8952712e4a7fff2ab6ba22d595e90a4a74d680c
mmya2300 <mmya230300@gmail.com>
2023-08-08 19:56:20 -0400

    Update - merged branches with main

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <Hero/>
       <Footer />
-=======
     <main className="h-full box-border m-0 p-0">
       <div className="contents">
         <div style={{minHeight:"100%", width:"auto"}}className="relative flex flex-col flex-nowrap gap-6 justify-start overflow-hidden p-0 min-h-full h-min">
@@ -17,7 +15,6 @@ function App() {
         </div>
       </div>
     </main>
->>>>>>> 1431b1b55b12e45678036081222423634504066a
     </>
   )
 }
