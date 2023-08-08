d6e8b56efcf96bb14f20a28c53173b22f108ce96
Jaden Lewis-Chambers <you@example.com>
2023-08-08 19:10:13 -0400

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
     <>
     <main className="h-full box-border m-0 p-0">
       <div className="contents">
-        <div style={{minHeight:"100%", width:"auto"}}className="relative flex  flex-col flex-nowrap gap-6 justify-start overflow-hidden p-0 min-h-full h-min">
+        <div style={{minHeight:"100%", width:"auto"}}className="relative flex flex-col flex-nowrap gap-6 justify-start overflow-hidden p-0 min-h-full h-min">
           <Hero/>
+          
         </div>
       </div>
     </main>
@@ -16,4 +17,3 @@ function App() {
 }
 
 export default App
-//relative flex flex-col flex-nowrap w-auto overflow-hidden justify-start h-min	gap-6
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,18 @@
 import {  motion } from 'framer-motion'
 
 
-const position = {
+function Bubble({ name, color }){
+  const position = {
     initial: {
       x: 0,
       y: 0,
     },
     final: {
-      x: -10,
-      y: 10,
+      x: -30,
+      y: 30,
       
     }
-}
-function Bubble(){
-
-
+  }
 
   return(
     <>
@@ -22,15 +20,18 @@ function Bubble(){
         className='z-1 absolute h-auto w-auto top-[-2%] left-[72%]'
         initial="initial" 
         animate="final"
-        transition={{ repeat: Infinity, repeatType: 'mirror', repeatDelay: 4, duration: 2 ,}}
+        transition={{ repeat: Infinity, repeatType: 'mirror', repeatDelay: 4, duration: 2 }}
         variants={position}
       >
         <div className='contents'>
           <div className='flex flex-row flex-none flex-nowrap overflow-visible items-center justify-start h-min'>
-            <div className='relative bg-pink-500 flex flex-none overflow-visible items-start justify-start rounded-xl px-4 py-2 h-min'>
+            <div className={`relative ${color} flex flex-none overflow-visible items-start justify-start rounded-xl px-4 py-2 h-min`}>
                 <div className='flex flex-col flex-none shrink-0 justify-start whitespace-pre transform-none h-auto w-auto '>
-                  <p className='font-bold'>Polina</p>
+                  <p className='font-bold'>{name}</p>
                 </div>
+            </div>
+            <div className=''>
+
             </div>
           </div>
         </div>
@@ -60,7 +61,10 @@ function Hero() {
               <button className="transition duration-200 hover:scale-105 bg-white font-bold rounded-xl py-4 px-8 mt-8">Become a member</button>
             {/* </div>s */}
           </div>
-          <Bubble/>
+          <Bubble
+            name="Polina"
+            color="bg-pink-500"
+          />
         </div>
         
       </section>
