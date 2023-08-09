83871d27c82a4db1293c6ec2497bb16937555924
mmya2300 <mmya230300@gmail.com>
2023-08-08 20:04:31 -0400

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ function App() {
   return (
     <>
       <Hero/>
-      <Footer />
+      
     <main className="h-full box-border m-0 p-0">
       <div className="contents">
         <div style={{minHeight:"100%", width:"auto"}}className="relative flex flex-col flex-nowrap gap-6 justify-start overflow-hidden p-0 min-h-full h-min">
-          <Hero/>
+
           
         </div>
+        <Footer />
       </div>
     </main>
     </>
