90c25dcdc18256e6e780e04b5a91a4cf3fb1cc85
mmya2300 <mmya230300@gmail.com>
2023-09-13 16:28:57 -0400

    Footer is being worked on

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,12 @@ function App() {
       
       <OfficerCarousel/>
       </div>
-    <main className="h-full box-border m-0 p-0">
-      <div className="contents">
-        <div style={{minHeight:"100%", width:"auto"}}className="relative flex flex-col flex-nowrap gap-6 justify-start overflow-hidden p-0 min-h-full h-min">
-          
-        </div>
+    <div className="h-full box-border m-0 p-0">
+      
         <Footer />
-      </div>
+      
 
-    </main>
+    </div>
     </>
     </div>
   );
