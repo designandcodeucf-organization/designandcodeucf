462575251453a30bd34aa2baa627ae0e27cf282e
kharidubard <kharidubard@gmail.com>
2023-09-18 15:02:13 -0400

    Update officerData.js

--- src/officerData.js
+++ src/officerData.js
@@ -32,7 +32,7 @@ export const data = [
       description:'As vice president of Design & Code, Alan is a huge collaborator. Born in Japan and raised in Canada and the U.S., he works hard to bring people together to learn about coding and create web designs. In his spare time, he builds cars, watches anime, and rides on his longboard.',
       socials:'Where to reach Alan:',
       icons: [
-        { socialName: 'LinkedIn', link: 'https://www.linkedin.com/' },
+        { socialName: 'LinkedIn', link: 'https://www.linkedin.com/in/alan-giraldo-87321b1b6/' },
       { socialName: 'GitHub', link: 'https://github.com/2jzAlan' },
       { socialName: 'Email', link: 'mailto:ar464589@ucf.edu' }
         // Add more social icons here
