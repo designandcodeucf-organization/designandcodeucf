3d97a87d34badfc9aa04fe9786e8b10f1b3a3b49
AndresJesusRodriguez <97657587+AndresJesusRodriguez@users.noreply.github.com>
2023-09-21 21:23:38 -0400

    Add NavBar from andres-branch links currently empty

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,28 @@ function NavBar(){
     const exampleElement = <h2 className="text-white">{name}</h2>
     
     return(
-        <>
-        <h1 className="text-white">navbar</h1>
-        <div>{exampleElement}</div>
-        </>
+        <nav className=" bg-opacity-100 flex flex-row justify-around float-right">
+        
+            {/* main page navbar links */}
+            <div className="m-8 ">
+                <ul className="flex items-center gap-[3vw] mx-auto">
+                    <li className="text-white transition hover:text-gray-500">
+                        <a href="#">Home</a>
+                    </li>
+                    <li className="text-white transition hover:text-gray-500">
+                        <a href="#officers">About Us</a>
+                    </li>
+                    <li className="text-white transition hover:text-gray-500">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            
+            {/* Sign up button */}
+            <div className="m-8">
+                <button className="text-white transition hover:text-gray-500 w-[89px] h-[34px] left-[1043.5px] top-[41.5px] rounded-full bg-[#252526]">Sign Up</button>
+            </div>
+        </nav>
     )
 }
 
