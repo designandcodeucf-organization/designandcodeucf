2810171ab66b1f4df15d7f4dc536712375e0811b
jaden416 <jaden416@outlook.com>
2023-09-25 21:17:04 -0400

    styling for socials

--- src/components/Offerings.jsx
+++ src/components/Offerings.jsx
@@ -1,34 +1,34 @@
 
 
-function About() {
+function Offerings() {
   return (  
     <>
       <section className=" my-[10rem] flex items-center justify-center w-full h-[min-content]">
-        <div className="gap-[1rem] flex s:flex-col max-w-[80rem] px-[3.2rem] ">
-          <div className="flex-col gap-[1rem] flex-nowrap  flex h-full">
-            <div className="flex-col flex-nowrap  flex h-full">
-              <div className="bg-[#1B1B1B] p-[3.2rem] rounded-xl flex-col flex gap-2">
+        <div className="gap-[1rem] flex s:flex-col max-w-[80rem] h-[min-content] px-[3.2rem] ">
+          <div className="flex-col gap-[1rem] flex-nowrap flex h-full ">
+            <div className="flex-col flex-nowrap flex grow">
+              <div className="bg-[#1B1B1B] p-[3.2rem] h-full rounded-xl flex-col flex gap-2">
                 <img className="w-[16rem] h-[16rem] rounded-xl"src="src/assets/javascript.png"></img>
                 <h2 className="text-white text-[1.6rem] font-semibold pt-3">Learn about Javascript</h2>
-                <p className="text-[#777777] text-md font-semibold">We are a UCF student club that specializes in web design. Dedicated and focused  creating content for the digital world. </p>
+                <p className="text-[#777777] text-md font-semibold">We are a UCF student club that specializes in web design.</p>
               </div>
             </div>
             <div className="flex-col flex-nowrap flex h-full">
-              <div className="bg-[#1B1B1B] p-[3.2rem] rounded-xl flex-col flex gap-2">
+              <div className="bg-[#1B1B1B] p-[3.2rem] h-full rounded-xl flex-col flex gap-2">
                 <h2 className="text-white  text-[1.6rem] font-semibold">Attend live workshops</h2>
                 <p className="text-[#777777] text-md font-semibold">We are a UCF student club that specializes in web design. Dedicated and focused  creating content for the digital world. </p>
               </div>
             </div>
           </div>
-          <div className="flex-col s:flex-col-reverse gap-[1rem] flex-nowrap  flex h-full">
+          <div className="flex-col s:flex-col-reverse gap-[1rem] flex-nowrap flex h-full">
             <div className="flex-col flex-nowrap  flex h-full">
-              <div className="bg-[#1B1B1B] p-[3.2rem] rounded-xl flex-col flex gap-2">
+              <div className="bg-[#1B1B1B] p-[3.2rem] h-full rounded-xl flex-col flex gap-2">
                 <h2 className="text-white text-[1.6rem] font-semibold">Learn about Javascript</h2>
                 <p className="text-[#777777] text-md font-semibold">We are a UCF student club that specializes in web design. Dedicated and focused  creating content for the digital world. </p>
               </div>
             </div>
             <div className="flex-col flex-nowrap  flex h-full">
-              <div className="bg-[#1B1B1B] p-[3.2rem] rounded-xl flex-col flex gap-2">
+              <div className="bg-[#1B1B1B] p-[3.2rem] h-full rounded-xl flex-col flex gap-2">
                 <img className="w-[16rem] h-[16rem] rounded-xl"src="src/assets/figma-logo.png"></img>
                 <h2 className="text-white text-[1.6rem] font-semibold pt-3">Learn about Javascript</h2>
                 <p className="text-[#777777] text-md font-semibold">We are a UCF student club that specializes in web design. Dedicated and focused  creating content for the digital world. </p>
@@ -42,4 +42,4 @@ function About() {
   )
 }
 
-export default About
+export default Offerings
