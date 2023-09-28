17f05b473e6a5d1d311f730a9eedc292e17e502e
jaden416 <jaden416@outlook.com>
2023-09-28 16:13:07 -0400

    star almost complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
           <div className="z-2 relative flex flex-col flex-none flex-nowrap content-center overflow-visible items-center justify-center h-min max-w-[44.8rem] w-full">
             <div className="relative flex flex-col flex-none flex-nowrap items-center justify-center gap-4 overflow-hidden h-min w-full">
               <div className="relative flex flex-col flex-none shrink-0	break-words whitespace-pre-wrap justify-start h-auto w-full">
-                <h1 className="text-white text-center text-lg font-bold">Introducing <br/>Design & Code</h1>
+                <h1 className="text-[white] text-center text-lg font-bold">Introducing <br/><span className="text-[#facc15]">Design & Code</span></h1>
               </div>
               <div className="relative flex flex-col flex-none shrink-0	break-words whitespace-pre-wrap justify-start h-auto w-full">
                 <p className="text-[#BBBBBB]  text-md text-center font-medium">We are a UCF student organization that specializes in web design. Dedicated and focused on creating content for the digital world. </p>
--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,27 +1,28 @@
-import { motion } from 'framer-motion';
+import { easeInOut, motion } from 'framer-motion';
 
-export default function Star({ width, hex, top, position, rotate }) {
+export default function Star({ width, hex, top, position, rotate, delay}) {
   return (
     <div className={`z-1 h-auto w-auto rotate-${rotate}`}>
       <div className={`absolute -top-${top} ${position}-0 animate-loadIn motion-reduce:animate-none`}>
         <motion.svg
-          width="52"
+          width={`${width}`}
           height="52"
           viewBox="0 0 52 52"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          className="w-11 text-amber-300 md:w-auto"
+          className="w-11 md:w-auto"
           animate={{
-            rotate: [0, 5, 0, -5, 0], // Define a rotation animation for shaking.
+            rotate: [-10, 10], // Define a rotation animation for shaking.
           }}
           transition={{
-            duration: 0.8,
+            duration: 2,
+            ease: easeInOut,
             repeat: Infinity, // Set the shake animation to repeat indefinitely.
           }}
         >
           <path
             d="M12.9683 16.4794C5.21439 18.3832 0 21.934 0 26C0 30.066 5.21439 33.6168 12.9683 35.5206C15.014 45.1704 20.0776 52 26 52C31.9224 52 36.986 45.1704 39.0317 35.5206C46.7856 33.6168 52 30.066 52 26C52 21.934 46.7856 18.3832 39.0317 16.4794C36.986 6.8296 31.9224 0 26 0C20.0776 0 15.014 6.82959 12.9683 16.4794Z"
-            fill="currentColor"
+            fill={`${hex}`}
           />
         </motion.svg>
       </div>
