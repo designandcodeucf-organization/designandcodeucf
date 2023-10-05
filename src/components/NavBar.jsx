72aef99b5468b70c6d3782cfdf975cc1b51e78ce
Jaden Lewis-Chambers <you@example.com>
2023-10-05 01:05:21 -0400

    navigation bar finished with bugs

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -41,8 +41,8 @@ function NavBar() {
           </div>
             <nav className={`${isActive ? "yo:opacity-1" : "yo:opacity-0"} fixed inset-0 z-40 h-[100vh] overflow-auto md:relative md:h-auto md:overflow-visible`}>
               <ul className="bg-black md:bg-transparent flex font-bold flex-col justify-center items-center h-full w-full  gap-5 md:flex-row md:justify-start  lg:flex-row">                
-                <a href="#Home" className="block lg:mt-0 text-white text-[1.6rem] hover:text-primaryLightBlue mr-4">Home</a>
-                <a href="#AboutUs" className="block lg:mt-0 text-white text-[1.6rem] hover:text-primaryLightBlue mr-4"> About Us</a>
+                <a onClick={toggleStyles} href="#Home" className="block lg:mt-0 text-white text-[1.6rem] hover:text-primaryLightBlue mr-4">Home</a>
+                <a onClick={toggleStyles} href="#AboutUs" className="block lg:mt-0 text-white text-[1.6rem] hover:text-primaryLightBlue mr-4"> About Us</a>
                 <a href="#Contact" className="block lg:mt-0 text-white text-[1.6rem] hover:text-primaryLightBlue mr-4"> Contact </a>
                 <a href="https://knightconnect.campuslabs.com/engage/organization/designandcode" className="inline-block text-sm px-4 py-2 leading-none border rounded-2xl text-white text-[1.6rem] border-white hover:border-transparent hover:text-white hover:bg-primaryLightBlue lg:mt-0">
                   Become A Member
