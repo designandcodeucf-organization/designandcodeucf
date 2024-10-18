c7cc6615f40219eb1f43aa687e5ad21c6be48e85
Beginners-Luck123 <dynieme.fms@gmail.com>
2024-10-17 23:58:31 -0400

    updated officers of fall 2024

--- src/officerData.js
+++ src/officerData.js
@@ -1,192 +1,130 @@
-export const data = [
-  {
-    id: 1,
-    img: "src/assets/officerHeadshots/Polina1600x1600.jpg",
-    name: "Polina Shliakhina",
-    position: "President",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "",
-    description:
-      "Polina is the president of Design & Code, dedicated to leadership and learning. Originally from Kyrgyzstan, she loves programming, learning new languages, and reading nonfiction. She also loves going outdoors, expressing this passion through the UCF Outdoor Adventure Program as a trip leader.",
-    socials: "Where to reach Polina:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/polinashliakhina/",
-      },
-      { socialName: "GitHub", link: "https://github.com/codedbypolina" },
-      { socialName: "Email", link: "mailto:ps.polina@knights.ucf.edu " },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 2,
-    img: "src/assets/officerHeadshots/Mya1600x1600.jpg",
-    name: " Mya Mosely",
-    position: "Vice President",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "Minor: Computer Science",
-    description:
-      "Mya is one of the event coordinators, dedicated to planning and securing spaces for events. Raised in Jacksonville, her determination and ambition show through her work in event organization and coding. She is minoring in Computer Science, and some of her favorite books are “Parable of the Sower,” and “Dune.”",
-    socials: "Where to reach Mya:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/mya-a-mosely/",
-      },
-      { socialName: "GitHub", link: "https://github.com/mmya2300" },
-      { socialName: "Email", link: "mailto:moselymya230300@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 3,
-    img: "src/assets/officerHeadshots/Sydney1600x1600.jpg",
-    name: "Sydney Sedloff",
-    position: "Project Manager",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "",
-    description:
-      "Sydney is Design & Code's treasurer, keeping track of the club's finances and budget. Born and raised in Central Florida, she is passionate about organization and project management. She loves reading fantasy books and owns a horse.",
-    socials: "Where to reach Sydney:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/sydney-sedloff/",
-      },
-      { socialName: "GitHub", link: "https://github.com/sydsedloff" },
-      { socialName: "Email", link: "mailto:sy884425@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 4,
-    img: "src/assets/officerHeadshots/Lindsay1600x1600.jpg",
-    name: "Lindsey Nielsen",
-    position: "Secretary",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "Minor: Computer Science",
-    description:
-      "Serving as the secretary of Design & Code, Lindsey is welcoming and organized. Originally from Seattle, she helps with facilitating meetings and holding important information for the club. She is passionate about music and cats, and is also a triplet",
-    socials: "Where to reach Lindsey:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/lindsey-nielsen-97217b206/",
-      },
-      { socialName: "GitHub", link: "https://github.com/lindsey-nielsen" },
-      { socialName: "Email", link: "mailto:lindsey.nielsen@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 5,
-    img: "src/assets/officerHeadshots/Khari2_1600x1600.jpg",
-    name: "Khari DuBard",
-    position: "Marketing Coordinator",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "",
-    description:
-      "Khari is one of the marketing coordinators, assisting with the overall presentation of the club. Living in Orlando for all of his life, he is passionate about digital art, and has even created Design & Code’s official logo. He loves geography and plays GeoGuessr in his free time.",
-    socials: "Where to reach Khari:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/khari-dubard-b90ba2248/",
-      },
-      { socialName: "GitHub", link: "https://github.com/kharidubard" },
-      { socialName: "Email", link: "mailto:kh689633@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 6,
-    img: "src/assets/officerHeadshots/Jolina1600x1600.jpg",
-    name: " Jolina Jassal",
-    position: "Marketing Coordinator",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "",
-    description:
-      "Jolina is another one of the marketing coordinators, establishing looks and aesthetics for the club. An Orlando resident, she has been into art and media for most of her life, such as reading, writing, singing, and more. She writes for the HerCampus at UCF chapter and loves chocolate.",
-    socials: "Where to reach Jolina:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/jolina-jassal-15ba681b4",
-      },
-      { socialName: "GitHub", link: "https://github.com/jolijass" },
-      { socialName: "Email", link: "mailto: jo625763@ucf.edu " },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 7,
-    img: "src/assets/officerHeadshots/Casi1600x1600.jpg",
-    name: "Casandra Alvarez",
-    position: "Marketing Coordinator",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "Minor: Art History",
-    description:
-      "As a marketing coordinator, Casi assists with the promotion of the club. She is very artistic and resourceful, being experienced with arts and crafts and providing important marketing information. She loves cozy games and works at the UCF Downtown Campus library.",
-    socials: "Where to reach Casi:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/casandra-alvarez/",
-      },
-      { socialName: "GitHub", link: "https://github.com/ca764763" },
-      { socialName: "Email", link: "mailto:ca764763@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
+/** @format */
 
-  {
-    id: 8,
-    img: "src/assets/officerHeadshots/matthew1600x1600.jpg",
-    name: "Matthew Sand",
-    position: "Event Coordinator",
-    readMore: "Click to learn more",
-    major: "Major: Computer Science",
-    minor: "",
-    description:
-      "Matthew is one of the event coordinators who has a passion for web development. He is eager to organize events that inspire others in the same field. In his free time, Matthew enjoys cooking, gaming, reading, and planning memorable moments with friends. ",
-    socials: "Where to reach Matthew:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/matthew-sand",
-      },
-      { socialName: "GitHub", link: "https://github.com/Quickzand" },
-      { socialName: "Email", link: "mailto:ma941728@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
-  {
-    id: 9,
-    img: "src/assets/officerHeadshots/Andres1600x1600.jpg",
-    name: "Andres Rodriguez",
-    position: "Community Builder",
-    readMore: "Click to learn more",
-    major: "Major: Digital Media - Web Design",
-    minor: "Minor: Computer Science",
-    description:
-      "Andres is the community builder of Design & Code, focused on creating a welcoming and fun environment for all members. Originally from South Florida, he has helped with organizing channels and social events, establishing a strong foundation for communication for the club.",
-    socials: "Where to reach Andres:",
-    icons: [
-      {
-        socialName: "LinkedIn",
-        link: "https://www.linkedin.com/in/andres-rodriguez-343b83273/",
-      },
-      { socialName: "GitHub", link: "https://github.com/AndresJesusRodriguez" },
-      { socialName: "Email", link: "mailto:an542918@ucf.edu" },
-      // Add more social icons here
-    ],
-  },
+export const data = [
+	{
+		id: 1,
+		img: "src/assets/officerHeadshots/meera.jpg",
+		name: "Meera Bhola",
+		position: "President",
+		readMore: "Click to learn more",
+		major: "Major: Digital Media - Web Design",
+		minor: "Spanish",
+		description:
+			"Meera's primarily interested in front-end development and her favorite framework right now is Bootstrap CSS. She has an interest in space and the effort it takes to achieve deep space exploration. In her free time she enjoys playing pickleball, reading, video games, and spending time with family and friends.",
+		socials: "Where to reach Meera:",
+		icons: [
+			{
+				socialName: "LinkedIn",
+				link: "https://www.linkedin.com/in/meera-bhola/",
+			},
+			{ socialName: "GitHub", link: "https://github.com/meera-develops" },
+			{ socialName: "Email", link: " " },
+			// Add more social icons here
+		],
+	},
+	{
+		id: 2,
+		img: "src/assets/officerHeadshots/Valeria.jpg",
+		name: "Valeria Tibocha",
+		position: "Vice President",
+		readMore: "Click to learn more",
+		major: "Major: Digital Media - Web Design",
+		minor: "Minor: Marketing",
+		description:
+			"Valeria was raised in Weston, FL and plan to combine her passion for design and marketing in the future. Her favorite games at the moment are Valorant and Lethal Company.”",
+		socials: "Where to reach Valeria:",
+		icons: [
+			{
+				socialName: "LinkedIn",
+				link: "https://www.linkedin.com/in/valeria-tibocha/",
+			},
+			{ socialName: "GitHub", link: "" },
+			{ socialName: "Email", link: "mailto:valeriaftibocha@gmail.com" },
+			// Add more social icons here
+		],
+	},
+	{
+		id: 3,
+		img: "src/assets/officerHeadshots/alyson.jpeg",
+		name: "Alyson Franco",
+		position: "Treasurer",
+		readMore: "Click to learn more",
+		major: "Major: Information Technology",
+		minor: "",
+		description:
+			"Alyson's hometown is Cape Coral, FL. She enjoys crocheting and playing horizon zero dawn games. She joined the club to explore more web development opportunities outside of the class.",
+		socials: "Where to reach Alyson:",
+		icons: [
+			{
+				socialName: "LinkedIn",
+				link: "https://www.linkedin.com/in/alysonfranco/",
+			},
+			{ socialName: "GitHub", link: "https://github.com/alysonfranco" },
+			{ socialName: "Email", link: "mailto:sy884425@ucf.edu" },
+			// Add more social icons here
+		],
+	},
+	{
+		id: 5,
+		img: "src/assets/officerHeadshots/Hadassah.jpeg",
+		name: "Hadassah Roberts",
+		position: "Marketing Coordinator",
+		readMore: "Click to learn more",
+		major: "Major: Digital Media - Web Design",
+		minor: "Computer Science",
+		description:
+			"Hadassah enjoys reading Silas Marner and “Frankenstein. She love the band MGMT and enjoys playing any of the Portal games and COD Zombies.",
+		socials: "Where to reach Hadassah:",
+		icons: [
+			{
+				socialName: "LinkedIn",
+				link: "",
+			},
+			{ socialName: "GitHub", link: "https://github.com/HadsRobs19" },
+			{ socialName: "Email", link: "mailto:hadassahplaysflute@gmail.com" },
+			// Add more social icons here
+		],
+	},
+	{
+		id: 8,
+		img: "src/assets/officerHeadshots/Dynie.jpg",
+		name: "Dynie Mesoneuvre",
+		position: "Event Coordinator",
+		readMore: "Click to learn more",
+		major: "Major: Digital Media - Web Design",
+		minor: "Computer Science",
+		description:
+			"Dynie likes to craft and go out on nature walks. She is graduating this Fall semester. Her favorite framework is React!",
+		socials: "Where to reach Dynie:",
+		icons: [
+			{
+				socialName: "LinkedIn",
+				link: "https://www.linkedin.com/in/dyniem/",
+			},
+			{ socialName: "GitHub", link: "" },
+			{ socialName: "Email", link: "mailto:dy918700@ucf.edu" },
+			// Add more social icons here
+		],
+	},
+	{
+		id: 9,
+		img: "src/assets/officerHeadshots/anna.jpg",
+		name: "Anna Russell",
+		position: "Community Builder",
+		readMore: "Click to learn more",
+		major: "Major: Computer Science",
+		minor: "",
+		description:
+			"Anna has a passion for learning new things and coding. She cares very deeply for her friends and family. On her free time she likes to read, draw, and play video games.",
+		socials: "Where to reach Anna:",
+		icons: [
+			{
+				socialName: "LinkedIn",
+				link: "",
+			},
+			{ socialName: "GitHub", link: "" },
+			{ socialName: "Email", link: "" },
+			// Add more social icons here
+		],
+	},
 ];
