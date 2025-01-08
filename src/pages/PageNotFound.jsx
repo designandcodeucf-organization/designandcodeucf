c6c446373bac0b9223619c4e40760e4e896d3cb4
hemkan <hemkano1403@gmail.com>
2025-01-08 14:10:11 -0500

    page not found formatting

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,6 +1,9 @@
 function PageNotFound() {
-    return <h1 className="text-white">Page Not Found</h1>;
+  return (
+    <h1 className="text-white text-center text-[4.2rem] text-5xl font-bold">
+      Page Not Found
+    </h1>
+  );
 }
-
 
 export default PageNotFound;
