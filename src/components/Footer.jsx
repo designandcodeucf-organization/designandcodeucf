38a8178ef01ac63edf5b465f26fa985483deaeee
hemkan <hemkano1403@gmail.com>
2025-01-09 14:04:41 -0500

    update data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,7 +88,7 @@ function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-[1.6rem] text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024{" "}
+          © 2025{" "}
           <a href="/" className="hover:underline">
             Design&Code™
           </a>
